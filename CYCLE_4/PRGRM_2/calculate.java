d232fa6a67b7bd93baf5eb1053d1b14b002e4b15
JERIL JOY <144669029+JERILJOY007@users.noreply.github.com>
2024-04-17 11:23:12 +0530

    Delete CYCLE_4/PRGRM_2 directory

--- CYCLE_4/PRGRM_2/calculate.java
+++
@@ -1,5 +0,0 @@
-package CO4Q2.operations;
-
-public interface calculate {
-    void cal(int x, int y);
-}
--- CYCLE_4/PRGRM_2/subtract.java
+++
@@ -1,8 +0,0 @@
-package CO4Q2.operations;
-
-public class subtract implements calculate{
-    public void cal(int x, int y){
-        int sub = x-y;
-        System.out.println("Difference of Numbers = "+sub);
-    }
-}
